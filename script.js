5611dfc170e7057ca576f91f88e6bceb25157ac8
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-03 17:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,27 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result=[];
+	let currSubArray=[];
+	let currSum=0;
+
+	for(num of arr){
+		if(currSum+num>n){
+			result.push(currSubArray);
+            currSubArray = []; 
+            currSum = 0;
+		}
+		currSubArray.push(num);
+        currSum += num;
+	}
+
+	if (currSubArray.length > 0) {
+        result.push(currSubArray);
+    }
+
+    return result;
+  
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
